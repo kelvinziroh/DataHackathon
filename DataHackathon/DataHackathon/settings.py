3d4888b21f3bc0022cd5c2dd9048cd64e10eb9d2
bkyalo <56555728+bkyalo@users.noreply.github.com>
2025-05-18 19:42:25 +0300

    Update CORS configuration to allow frontend domains and enable credentials

--- DataHackathon/DataHackathon/settings.py
+++ DataHackathon/DataHackathon/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '102.210.149.248', 'alx.snap.co.ke']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware should be as high as possible
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -176,12 +178,19 @@ CSRF_TRUSTED_ORIGINS = [
     'http://alx.snap.co.ke',
 ]
 
+# CORS settings
 CORS_ALLOWED_ORIGINS = [
     'https://alx.snap.co.ke',
     'http://alx.snap.co.ke',
     'http://localhost:5173',
     'https://datathon-sigma.vercel.app',
 ]
+
+# Allow credentials (cookies, authorization headers, etc.) to be included in cross-site requests
+CORS_ALLOW_CREDENTIALS = True
+
+# Allow all headers and methods
+CORS_ALLOW_ALL_ORIGINS = DEBUG  # Only allow all origins in DEBUG mode for security
 
 # Django allauth config
 SITE_ID = 1
